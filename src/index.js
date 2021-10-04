76f566e18712ce62a2568eb1f06db8681416c260
ironoa <alessio.onori@gmail.com>
2021-10-04 14:32:43 +0200

    fixed middleware

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ const app = express()
 const port = 8080
 const mongo_express_config = require('./config.js')
 
-/* Endpoint for healthchecks. */
-app.get('/__healthcheck__', (req, res) => res.send('Up and running!'));
+async function bootstrap() {
+  const resolvedMiddleware = await mongo_express(mongo_express_config);
+ 
+  /* Endpoint for healthchecks. */
+  app.get('/__healthcheck__', (req, res) => res.send('Up and running!'));
 
-app.use('/', mongo_express(mongo_express_config))
+  app.use('/', resolvedMiddleware)
 
-app.listen(port, () => console.log(`mongo-express listening on port ${port}!`))
+  app.listen(port, () => console.log(`mongo-express listening on port ${port}!`))
+}
+bootstrap()
